faae2195cccb47397d493929577440162aa37f62
Facultad de Contaduría y Administración <facultaddecontaduriayadministracion@MacBook-4.local>
2024-03-06 10:59:33 -0600

    avance

--- CalculadoraV1.playground/Contents.swift
+++ CalculadoraV1.playground/Contents.swift
@@ -1,13 +1,30 @@
-import UIKit
+
 
 struct calculadora{
     
-    var num1, num2: Int
+    //var num1, num2: Double
     
-    func operacionSuma(num1: Int, num2: Int) ->Int{
+    func operacionSuma(num1: Double, num2: Double) ->Double{
         
         return num1 + num2
     }
     
-
+    func operacionResta(num1: Double, num2:Double) ->Double{
+        
+        return num1 - num2
+        
+    }
+    
+    func operacionDivision(num1: Double, num2:Double)-> Double{
+        
+        
+    }
+    
+    
+    
+    
 }
+
+let ccalucladora = calculadora()
+
+print(calculadora.operacionSuma(num1: 2.3, num2: 2.3))

0ddad51ade93d881a4c81e69d2426780a9120001
Facultad de Contaduría y Administración <facultaddecontaduriayadministracion@MacBook-4.local>
2024-03-06 10:08:13 -0600

    calculadora

---
+++ CalculadoraV1.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+struct calculadora{
+    
+    var num1, num2: Int
+    
+    func operacionSuma(num1: Int, num2: Int) ->Int{
+        
+        return num1 + num2
+    }
+    
+
+}

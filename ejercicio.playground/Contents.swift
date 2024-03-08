bd011b6bdb615c8080b913ed25cc1618e062223e
Facultad de Contaduría y Administración <facultaddecontaduriayadministracion@MacBook-21.local>
2024-03-08 09:45:16 -0600

    mi primer gitpush

--- ejercicio.playground/Contents.swift
+++ ejercicio.playground/Contents.swift
@@ -1,2 +1,10 @@
 import UIKit
 
+struct Calculadora{
+    func suma(x1: Double, x2: Double) -> Double{
+        return x1 + x2
+    }
+}
+
+let miCalculadora = Calculadora()
+print(miCalculadora.suma(x1: 1, x2: 1)) //2

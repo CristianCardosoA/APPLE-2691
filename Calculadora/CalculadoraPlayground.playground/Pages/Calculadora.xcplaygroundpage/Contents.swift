61ad0c2d33e0c6a01645b4a4ebb4dabe8590ece1
Cristian Cardoso <cristian.cardoso@dexcom.com>
2024-03-08 09:12:18 -0800

    2nda version calc

---
+++ Calculadora/CalculadoraPlayground.playground/Pages/Calculadora.xcplaygroundpage/Contents.swift
@@ -0,0 +1,58 @@
+enum CalculadoraError {
+    case divZero
+    case maxNum
+    case minNum
+}
+
+enum Operacion {
+    case sum
+    case res
+    case mul
+    case div
+}
+
+protocol DisplayProtocol {
+    func muestraResultado(resultado: Double) -> String
+    func muestraNumero(numero: Double) -> String
+    func muestraError(error: CalculadoraError) -> String
+}
+
+protocol ProcesamientoProtocol {
+    func sum(x: Double, y: Double) -> Double
+    func res(x: Double, y: Double) -> Double
+    func mul(x: Double, y: Double) -> Double
+    func div(x: Double, y: Double) -> Double
+}
+
+protocol CacheProtocol {
+    func guardaNum(num: Int) -> Double
+    func guardaOp(op: Operacion)
+}
+
+protocol ControlProtocol {
+    func igual() -> Double
+}
+
+protocol CalculadoraProtocol { }
+
+struct Display: DisplayProtocol {
+    func muestraResultado(resultado: Double) -> String {
+        "AUN NO ESTA IMPLEMENTADO 🤬"
+    }
+    
+    func muestraNumero(numero: Double) -> String {
+        "AUN NO ESTA IMPLEMENTADO 🤬"
+    }
+    
+    func muestraError(error: CalculadoraError) -> String {
+        "AUN NO ESTA IMPLEMENTADO 🤬"
+    }
+}
+
+struct Calculadora: CalculadoraProtocol {
+    
+    var display: DisplayProtocol = Display()
+    var cache: CacheProtocol = Cache()
+    var control: ControlProtocol = Control()
+    var procesamiento: ProcesamientoProtocol = Procesamiento()
+}

01d365d20da493a8a37ed297172cce98d67843f6
Facultad Contaduría y Administración <facultadcontaduriayadministracion@MacBook-13.local>
2024-03-08 09:39:21 -0600

    Calculadora avance

---
+++ Calculadora.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+func suma(_ numero1: Double, _ numero2: Double) -> Double{
+    
+    return numero1 + numero2
+}
+
+func resta(_ numero1: Double, _ numero2: Double) -> Double{
+    
+    return numero1 - numero2
+}
+
+func multiplicacion(_ numero1: Double, _ numero2: Double) -> Double{
+    
+    return numero1 * numero2
+}
+
+func division(_ numero1: Double, _ numero2: Double) -> Double{
+    
+    return numero1 / numero2
+}
+
+

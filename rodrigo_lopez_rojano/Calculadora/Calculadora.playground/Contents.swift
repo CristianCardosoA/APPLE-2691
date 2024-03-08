7bef93126416053ccb15db6f2fe60fb7d6108dc5
Facultad de Contaduría y Administración <facultaddecontaduriayadministracion@MacBook-14.local>
2024-03-08 09:52:01 -0600

    Agregue el gitignore y programa Calculadora

---
+++ rodrigo_lopez_rojano/Calculadora/Calculadora.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+struct Calculadora {
+    func suma (x1: Double, x2: Double) -> Double {
+        return x1 + x2
+    }
+        func resta(x1: Double, x2: Double) -> Double {
+            return x1 - x2
+    }
+            func division(x1: Double, x2: Double) -> Double {
+                return x1 / x2
+    }
+                func multiplicacion (x1: Double, x2: Double) -> Double {
+                    return x1 * x2
+    }
+}
+        

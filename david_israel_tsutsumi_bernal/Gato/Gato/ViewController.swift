04ae5a0a3b0ac2af53a21c9eec265301c5ef8c6a
David Israel Tsutsumi Bernal <tsutsumi@MacBook-Air-de-David.local>
2024-03-15 11:11:11 -0600

    PROYECTO GATO

---
+++ david_israel_tsutsumi_bernal/Gato/Gato/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Gato1.1
+//
+//  Created by Facultad de Contaduría y Administración on 15/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var arreglo = [Int]()
+    
+    var temporal = 0
+    
+    @IBAction func onTap(_ sender: Any) {
+        
+        if temporal < 100{
+            labelNombre.text = "\(arreglo[temporal])"
+            temporal += 1
+        } else{
+            temporal = 0
+        }
+    }
+    
+    @IBOutlet weak var labelNombre: UILabel!
+    
+    func inicializador(){
+         
+        for i in 1...100{
+            
+            arreglo.append(i)
+        }
+        
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        inicializador()
+        
+        
+    }
+
+
+}

9b2987cd5181e1017d17584eee90191434091d4f
Facultad de Contaduría y Administración <facultaddecontaduriayadministracion@MacBook-7.local>
2024-03-21 12:43:01 -0600

    tarea

--- Gato2/Gato2/ViewController.swift
+++ Gato2/Gato2/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
     @IBOutlet weak var img1: UIImageView!
     
     @IBOutlet weak var img2: UIImageView!
@@ -26,101 +27,88 @@ class ViewController: UIViewController {
     @IBOutlet weak var img8: UIImageView!
     
     @IBOutlet weak var img9: UIImageView!
+
     
-    var esSol = false
+    var esCara = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         agregarGestos()
     }
 
     func agregarGestos(){
-        let tap1 = UITapGestureRecognizer(target: self, action: #selector(self.tapimg1))
+        let tap1 = UITapGestureRecognizer(target: self, action: #selector(self.tapImg1))
         img1.isUserInteractionEnabled = true
         img1.addGestureRecognizer(tap1)
         
-        let tap2 = UITapGestureRecognizer(target: self, action: #selector(self.tapimg2))
+        let tap2 = UITapGestureRecognizer(target: self, action: #selector(self.tapImg2))
         img2.isUserInteractionEnabled = true
         img2.addGestureRecognizer(tap2)
         
-        let tap3 = UITapGestureRecognizer(target: self, action: #selector(self.tapimg3))
+        let tap3 = UITapGestureRecognizer(target: self, action: #selector(self.tapImg3))
         img3.isUserInteractionEnabled = true
         img3.addGestureRecognizer(tap3)
         
-        let tap4 = UITapGestureRecognizer(target: self, action: #selector(self.tapimg4))
+        let tap4 = UITapGestureRecognizer(target: self, action: #selector(self.tapImg4))
         img4.isUserInteractionEnabled = true
         img4.addGestureRecognizer(tap4)
         
-        let tap5 = UITapGestureRecognizer(target: self, action: #selector(self.tapimg5))
+        let tap5 = UITapGestureRecognizer(target: self, action: #selector(self.tapImg5))
         img5.isUserInteractionEnabled = true
         img5.addGestureRecognizer(tap5)
         
-        let tap6 = UITapGestureRecognizer(target: self, action: #selector(self.tapimg6))
+        let tap6 = UITapGestureRecognizer(target: self, action: #selector(self.tapImg6))
         img6.isUserInteractionEnabled = true
         img6.addGestureRecognizer(tap6)
         
-        let tap7 = UITapGestureRecognizer(target: self, action: #selector(self.tapimg7))
+        let tap7 = UITapGestureRecognizer(target: self, action: #selector(self.tapImg7))
         img7.isUserInteractionEnabled = true
         img7.addGestureRecognizer(tap7)
         
-        let tap8 = UITapGestureRecognizer(target: self, action: #selector(self.tapimg8))
+        let tap8 = UITapGestureRecognizer(target: self, action: #selector(self.tapImg8))
         img8.isUserInteractionEnabled = true
         img8.addGestureRecognizer(tap8)
         
-        let tap9 = UITapGestureRecognizer(target: self, action: #selector(self.tapimg9))
+        let tap9 = UITapGestureRecognizer(target: self, action: #selector(self.tapImg9))
         img9.isUserInteractionEnabled = true
         img9.addGestureRecognizer(tap9)
+        
     }
-
-    @objc func tapimg1(_ sender: UITapGestureRecognizer){
-        print("🐶 clic 1")
-        img1.image = esSol ? UIImage(named: "mon20pesosC1_Zapata_anv-1") :UIImage(named: "mon20pesosC1_Zapata_anv-1") esSol.toggle()
+    @objc func tapImg1(_ sender: UITapGestureRecognizer){
+        img1.image = esCara ? UIImage(named: "cara") :UIImage(named: "aguila")
+        esCara.toggle()
     }
-    
-    @objc func tapimg2(_ sender: UITapGestureRecognizer){
-        print("🐶 clic 2")
-
+    @objc func tapImg2(_ sender: UITapGestureRecognizer){
+        img2.image = esCara ? UIImage(named: "cara") :UIImage(named: "aguila")
+        esCara.toggle()
     }
-    
-    @objc func tapimg3(_ sender: UITapGestureRecognizer){
-        print("🐶 clic 3")
-
+    @objc func tapImg3(_ sender: UITapGestureRecognizer){
+        img3.image = esCara ? UIImage(named: "cara") :UIImage(named: "aguila")
+        esCara.toggle()
     }
-    
-    @objc func tapimg4(_ sender: UITapGestureRecognizer){
-        print("🐶 click 4")
-)
-
-
+    @objc func tapImg4(_ sender: UITapGestureRecognizer){
+        img4.image = esCara ? UIImage(named: "cara") :UIImage(named: "aguila")
+        esCara.toggle()
     }
-    
-    @objc func tapimg5(_ sender: UITapGestureRecognizer){
-        print("🐶 clic 5")
-
+    @objc func tapImg5(_ sender: UITapGestureRecognizer){
+        img5.image = esCara ? UIImage(named: "cara") :UIImage(named: "aguila")
+        esCara.toggle()
     }
-    
-    @objc func tapimg6(_ sender: UITapGestureRecognizer){
-        print("🐶 clic 6")
-
+    @objc func tapImg6(_ sender: UITapGestureRecognizer){
+        img6.image = esCara ? UIImage(named: "cara") :UIImage(named: "aguila")
+        esCara.toggle()
     }
-    
-    @objc func tapimg7(_ sender: UITapGestureRecognizer){
-        print("🐶 clic 7")
-
+    @objc func tapImg7(_ sender: UITapGestureRecognizer){
+        img7.image = esCara ? UIImage(named: "cara") :UIImage(named: "aguila")
+        esCara.toggle()
     }
-    
-    @objc func tapimg8(_ sender: UITapGestureRecognizer){
-        print("🐶 clic 8")
-
+    @objc func tapImg8(_ sender: UITapGestureRecognizer){
+        img8.image = esCara ? UIImage(named: "cara") :UIImage(named: "aguila")
+        esCara.toggle()
     }
-    
-    @objc func tapimg9(_ sender: UITapGestureRecognizer){
-        print("🐶 clic 9")
-
+    @objc func tapImg9(_ sender: UITapGestureRecognizer){
+        img9.image = esCara ? UIImage(named: "cara") :UIImage(named: "aguila")
+        esCara.toggle()
     }
-    
-    
-    
 }
-

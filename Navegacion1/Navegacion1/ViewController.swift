894be563caf9a27d00742029ae1b302cb8576bf5
Facultad Contaduría y Administración <facultadcontaduriayadministracion@MacBook-13.local>
2024-04-10 11:03:01 -0600

    Actividad navegacion

--- Navegacion1/Navegacion1/ViewController.swift
+++ Navegacion1/Navegacion1/ViewController.swift
@@ -14,6 +14,34 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onTap(_ sender: Any){
+        performSegue(withIdentifier: "primerSegue", sender: nil)
+    }
+    
+    @IBAction func BotonA(_ sender: Any) {
+        performSegue(withIdentifier: "uno", sender: nil)
+    }
+    
+    @IBAction func BotonB(_ sender: Any) {
+        performSegue(withIdentifier: "dos", sender: nil)
+    }
+    
+    @IBAction func BotonC(_ sender: Any) {
+        performSegue(withIdentifier: "tres", sender: nil)
+    }
+    
+    @IBAction func BotonD(_ sender: Any) {
+        performSegue(withIdentifier: "cuatro", sender: nil)
+    }
+    
+    @IBAction func BotonE(_ sender: Any) {
+        performSegue(withIdentifier: "cinco", sender: nil)
+    }
+    
+    @IBAction func BotonA1(_ sender: Any) {
+        performSegue(withIdentifier: "seis", sender: nil)
+    }
+    
+    
 }
 

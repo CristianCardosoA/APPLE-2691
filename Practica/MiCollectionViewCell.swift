0ab0efaa3af5ab4aacb0d95a1970ba106564e06f
Facultad de Contaduría y Administración <facultaddecontaduriayadministracion@MacBook-7.local>
2024-04-19 10:00:10 -0600

    aldooooo

---
+++ Practica/MiCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MiCollectionViewCell.swift
+//  Practica
+//
+//  Created by Facultad de Contaduría y Administración on 19/04/24.
+//
+
+import Foundation
+import UIKit
+
+class MiCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var Titulo: UILabel!
+    
+    
+    
+}
---
+++ fibonacci/fibonacci/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  fibonacci
+//
+//  Created by Facultad de Contaduría y Administración on 03/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var x = 0
+    var y = 1
+    var z = 1
+    var temporal = 0
+    
+    func fibo(x: Int){
+        while temporal > 10000
+        
+        
+                
+        
+    }
+    
+    
+    @IBAction func menos(_ sender: Any) {
+    }
+    
+    @IBAction func mas(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+    
+}
+
+
+

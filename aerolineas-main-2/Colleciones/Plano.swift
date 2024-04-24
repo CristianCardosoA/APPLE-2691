6fdfb49317e7dd0353a9b82b7ea4f2eca68540e6
Facultad de Contaduría y Administracíon <facultaddecontaduriayadministracion@MacBook-2.local>
2024-04-24 10:23:26 -0600

    soy un chango marangoooo

---
+++ aerolineas-main-2/Colleciones/Plano.swift
@@ -0,0 +1,65 @@
+//
+//  Plano.swift
+//  Colleciones
+//
+//  Created by Cristian Cardoso on 18/04/24.
+//
+
+import Foundation
+
+
+struct Plano {
+    
+    var aviones: [Avion]
+
+    
+    var columnas: Int {
+        // Calcula tus columnas
+        // Nota> depende de tus aviones y tus coordenadas max y min.
+        return MaxX()
+    }
+    
+    var filas: Int {
+        // Calcula tus files
+        // Nota> depende de tus aviones y tus coordenadas max y min.
+        return MaxY()
+    }
+
+    func getAvion(row: Int, section: Int) -> Avion? {
+        return aviones.first { avion in
+            avion.x == row && avion.y == section
+        }
+    }
+    
+    mutating func next() -> Self {
+        // Implementa
+        aviones.removeAll()
+        return self
+    }
+    
+    mutating func back() -> Self {
+        // Implementa
+        aviones.removeAll()
+        return self
+    }
+    
+    func MaxY() -> Int {
+        var MaxFil: Int = 0
+        for avion in aviones{
+            if MaxFil < avion.y {
+                MaxFil = avion.y
+            }
+        }
+    }
+    
+    func MaxX() -> Int {
+        var MaxCol: Int = 0
+        for avion in aviones{
+            if MaxCol < avion.x {
+                MaxCol = avion.x
+            }
+        } 
+    }
+    
+    
+}
